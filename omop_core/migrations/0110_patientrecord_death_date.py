f20bfa4ab5716ec5a4beb9c253f9f624d179588d
Adam Blum <adam@cancerbot.org>
2026-07-16 14:30:02 +0100

    Fix idempotent death date migration

--- omop_core/migrations/0110_patientrecord_death_date.py
+++ omop_core/migrations/0110_patientrecord_death_date.py
@@ -8,13 +8,26 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='patientrecord',
-            name='death_date',
-            field=models.DateField(
-                blank=True,
-                help_text='Date of death (from OMOP Death)',
-                null=True,
-            ),
+        migrations.SeparateDatabaseAndState(
+            database_operations=[
+                migrations.RunSQL(
+                    sql=(
+                        'ALTER TABLE patient_record '
+                        'ADD COLUMN IF NOT EXISTS death_date date NULL'
+                    ),
+                    reverse_sql=migrations.RunSQL.noop,
+                ),
+            ],
+            state_operations=[
+                migrations.AddField(
+                    model_name='patientrecord',
+                    name='death_date',
+                    field=models.DateField(
+                        blank=True,
+                        help_text='Date of death (from OMOP Death)',
+                        null=True,
+                    ),
+                ),
+            ],
         ),
     ]
