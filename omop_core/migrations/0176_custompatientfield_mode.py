e6b1c1011b655b19b88f26e30d87ca1c9eb9269c
Adam Blum <adam@dplrn.com>
2026-08-26 12:32:08 +0100

    fix: rebase custom field mode migration on dev

--- omop_core/migrations/0176_custompatientfield_mode.py
+++ omop_core/migrations/0176_custompatientfield_mode.py
@@ -3,7 +3,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [('omop_core', '0174_custom_patient_fields')]
+    dependencies = [('omop_core', '0175_add_remission_duration')]
 
     operations = [
         migrations.AddField(
