cf687ce46bd498b55402c0291c09d93bdf5c98d6
Adam Blum <adam@dplrn.com>
2026-09-14 09:20:28 +0100

    fix: merge blood count and genomics migration branches (#1295)

---
+++ omop_core/migrations/0234_merge_blood_count_and_genomics.py
@@ -0,0 +1,12 @@
+"""Join the independently merged blood-count units and PALB2 migrations."""
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("omop_core", "0233_blood_count_units_640"),
+        ("omop_core", "0233_genomics_palb2_naming"),
+    ]
+
+    operations = []
